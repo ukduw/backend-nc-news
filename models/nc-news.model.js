d733906c9d48b588995cd4bef0cb3ae647b59300
ukduw <edhkmayo@gmail.com>
2025-03-10 12:21:23 +0000

    4-get-articles

--- models/nc-news.model.js
+++ models/nc-news.model.js
@@ -24,8 +24,13 @@ function fetchArticleById(article_id) {
     })
 }
 
+function fetchArticles() {
+    return db.query(`SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.comment_id) AS comment_count FROM articles JOIN comments ON articles.article_id = comments.article_id GROUP BY articles.article_id ORDER BY articles.created_at DESC`).then(({rows}) => {
+        return rows
+    })
+}
 
 
 
 
-module.exports = {fetchTopics, checkArticleIdExists, fetchArticleById}
+module.exports = {fetchTopics, checkArticleIdExists, fetchArticleById, fetchArticles}
