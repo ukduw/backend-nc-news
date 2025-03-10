1f4db69e607fd0e4f4e73c759ad083c3a15fe6b7
ukduw <edhkmayo@gmail.com>
2025-03-10 11:55:05 +0000

    3-get-article-by-id

--- models/nc-news.model.js
+++ models/nc-news.model.js
@@ -8,8 +8,24 @@ function fetchTopics() {
     })
 }
 
+function checkArticleIdExists(article_id) {
+    return db.query(`SELECT * FROM articles WHERE article_id = $1`, [article_id])
+    .then(({rows}) => {
+        if(rows.length === 0) {
+            return Promise.reject({status:404, msg: 'not found'})
+        }
+    })
+}
+
+function fetchArticleById(article_id) {
+    return db.query(`SELECT * FROM articles WHERE article_id = $1`, [article_id])
+    .then(({rows}) => {
+        return rows[0]
+    })
+}
 
 
 
 
-module.exports = {fetchTopics}
+
+module.exports = {fetchTopics, checkArticleIdExists, fetchArticleById}
